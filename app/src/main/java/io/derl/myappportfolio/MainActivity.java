f44c506c80d81eaf2d52c9ea88dd72cae37bedd6
derlio <derlio1024@gmail.com>
2016-04-27 17:06:45 +0800

    Impl the base activity

--- app/src/main/java/io/derl/myappportfolio/MainActivity.java
+++ app/src/main/java/io/derl/myappportfolio/MainActivity.java
@@ -1,7 +1,9 @@
 package io.derl.myappportfolio;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void spotifyStreamer(View view) {
+        showToast("spotifyStreamer");
+    }
+
+    public void superDuo1(View view) {
+        showToast("superDuo1");
+    }
+
+    public void superDuo2(View view) {
+        showToast("superDuo2");
+    }
+
+    public void buildItBigger(View view) {
+        showToast("buildItBigger");
+    }
+
+    public void xyzReader(View view) {
+        showToast("xyzReader");
+    }
+
+    public void capstone(View view) {
+        showToast("capstone");
+    }
+
+    private void showToast(String text){
+        Toast.makeText(this, "This button will launch " + text + " app!", Toast.LENGTH_SHORT).show();
     }
 }
